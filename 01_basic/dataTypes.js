79021a7dee214cd98b76d034f0a9e75435139331
anujpal247 <125887480+anujpal247@users.noreply.github.com>
2023-12-28 18:08:00 +0000

    learn data type

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,15 @@
+// number => 2 to power 53
+// bigInt
+// String => ""
+// boolean => true/false
+// null => type and value  (null is a object)
+// undefined
+
+let age = 20
+let name = "Anuj"
+let isloggedIn = false
+let state
+let temp = null 
+
+console.log(typeof(age));
+
